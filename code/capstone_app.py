71aac7783d21ba6750b9c833c7cbd157009e47f2
Elijah Curme <elijahcurme@S010660a44c5ea0e4.ed.shawcable.net>
2020-03-03 14:02:07 -0500

    updating cap

--- code/capstone_app.py
+++ code/capstone_app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, Response, render_template
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
-testinc = pd.read_csv('../data/testinc.csv')
+i = -1
 
 app = Flask('Capstone')
 
@@ -19,13 +19,38 @@ def form():
 @app.route('/submit')
 
 def submit():
+    global i
+    i += 1
+    print(i)
+    testinc = pd.read_csv('../data/testinc.csv')
+    
     user_input = request.args
-    print(*user_input, sep='\n')
+    #print(*user_input, sep='\n')
     
-    home_budget = float(user_input['home_budget'])
-    state = user_input['state']
+    if i == 0:
+        global home_budget
+        home_budget = float(user_input['home_budget'])
+        global state 
+        state = user_input['state']
+        state = state[0].upper() + state[1:]
     
-    return render_template('results.html')
+    crime_rate_ntl_avg = np.mean(testinc['crime_rate_pc'])
+    
+    testinc = testinc.loc[(testinc['state'] == state) & (testinc['home_price'] < home_budget)]
+    testinc.sort_values(by = 'residuals', ascending = False, inplace = True)
+    
+    best_deal = testinc.iloc[i]
+    
+    return render_template(
+        'results1.html',
+        townstate = best_deal['townstate'],
+        actual_home_price = int(best_deal['home_price']),
+        predicted_home_price = int(best_deal['preds']),
+        crime_rate_pc = best_deal['crime_rate_pc'],
+        relative_crime_rate = np.round(((crime_rate_ntl_avg - best_deal['crime_rate_pc']) / crime_rate_ntl_avg) * 100, 1),
+        unemployment_rate = np.round(best_deal['unemployment_rate'], 2),
+        median_income = int(best_deal['med_income'])
+    )
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0')
